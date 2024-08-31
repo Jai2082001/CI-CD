6867e38a27011c49dcb1a7d7afc55fb7d3f2dbc4
jaideep grover <jaideepgrover147@gmail.com>
2024-08-31 18:45:21 -0400

    adad

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,14 +2,9 @@ import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 export default async function Page(){
+  // asdasdasdad
   const session = await getServerSession(authOptions)
   if(session?.user){
     redirect('/dashboard');
