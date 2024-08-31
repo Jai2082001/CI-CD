3b26b653728d8d6b30c7bc9ae6c41d5edfaaf0c5
jaideep grover <jaideepgrover147@gmail.com>
2024-08-31 18:21:46 -0400

    main-branch githubworkflow updated

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,7 +2,10 @@ import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 export default async function Page(){
   const session = await getServerSession(authOptions)
   if(session?.user){
