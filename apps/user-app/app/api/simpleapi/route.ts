4005ea42a8440c3d0e6e224adaa44ec21fd4ff69
jaideep grover <jaideepgrover147@gmail.com>
2024-09-02 21:33:23 -0400

    sample api changed

--- apps/user-app/app/api/simpleapi/route.ts
+++ apps/user-app/app/api/simpleapi/route.ts
@@ -4,5 +4,5 @@ export const GET = async (req: NextRequest) => {
 
     console.log(req.headers)
 
-    return NextResponse.json({message: 'captured'})
+    return NextResponse.json({message: 'captured  1212213'})
 }
